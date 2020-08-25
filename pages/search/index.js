a5059621e7a72b3061b43870561dd334f9d1aac8
William5920 <1101044179@qq.com>
2020-08-25 15:05:00 +0800

    1.完成搜索中心相关功能。

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,66 +1,52 @@
 // pages/search/index.js
+import {request} from '../../request/index.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goods: [],
+    isFocus: false,
+    inputValue: ''
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  TimeId: -1,
+  handleInput(e) {
+    // console.log(e)
+    const {value} = e.detail
+    // 防抖
+    clearTimeout(this.TimeId)
+    // 检验字符串的合法性
+    if(!value.trim()) {
+      this.setData({
+        goods: [],
+        isFocus: false
+      })
+      
+      return
+    }
+    this.setData({isFocus: true})
+    // 合法后发送请求获取数据
+    // clearTimeout(this.TimeId)
+    this.TimeId = setTimeout(() => {
+      // console.log(value)
+      this.qsearch(value)
+    }, 1200)
+    
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  async qsearch(query) {
+    const res = await request({url: '/goods/qsearch', data: {query}})
+    // console.log(res)
+    if(res.statusCode === 200) {
+      this.setData({goods: res.data.message})
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleCancel() {
+    this.setData({
+      goods: [],
+      isFocus: false,
+      inputValue: ''
+    })
   }
+  
 })
